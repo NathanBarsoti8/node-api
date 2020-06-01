d9f260fc97591ab43280a8a58d19d528bbb87680
Nathan Barsoti <nathan.barsoti@programmers.com.br>
2020-05-31 23:26:42 -0300

    added cors and bodyParser

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 require('./database');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
 
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
