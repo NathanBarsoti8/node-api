e0fd23be658a0185d32b03cb4525e880ea2f0809
Nathan Barsoti <nathan.barsoti@programmers.com.br>
2020-06-01 20:18:33 -0300

    fixed server.js file

--- src/server.js
+++ src/server.js
@@ -5,16 +5,18 @@ const bodyParser = require('body-parser');
 
 require('./database');
 
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
 
-const app = express();
-const port = 3000;
-
-app.use(cors())
-app.use(express.json());
-app.use(routes);
-
-app.listen(port);
+app.listen(port, function() {
+    console.log(`API LISTENING ON PORT ${port}`);
+});
