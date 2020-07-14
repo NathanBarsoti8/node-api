a6299ae9deeb869ce4d97b3ebe2df94524fc3343
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-07-14 19:03:35 -0300

    some changes in method update schedule

--- src/controllers/SchedulingController.js
+++ src/controllers/SchedulingController.js
@@ -43,7 +43,7 @@ class SchedulingController {
             if (invalidTime)
                 return res.status(400).send({ msg: 'Nesse horário já existe uma consulta' });
 
-            else {
+            else if (!invalidTime) {
                 Scheduling.create(scheduling)
                     .then(result => {
                         if (result)
@@ -56,17 +56,76 @@ class SchedulingController {
         });
     }
 
-    update(req, res) {
+    async update(req, res) {
+        if (req.body.date) {
+            const today = moment(new Date(), "DD-MM-YYYY");
+            const date = moment(new Date(req.body.date), "DD-MM-YYYY");
+
+            if (date < today)
+                return res.status(400).send({ msg: 'Data da consulta menor que a data atual' })
+        }
+
         Scheduling.findOne({
             where: {
                 id: req.params.id
             }
         })
-        .then(scheduling => {
-            if (scheduling) {
-                scheduling.update(req.body);
-                
-                return res.status(200).send({ msg: 'Consulta atualizada com sucesso' });
+        .then(schedule => {
+            if (req.body.timeTable) {
+
+                let dateFilter;
+                let invalidTime;
+            
+                if (req.body.date)
+                    dateFilter = req.body.date
+                else
+                    dateFilter = schedule[0].date
+
+                await Scheduling.findAll({
+                    where: dateFilter
+                })
+                .then(schedules => {
+                    if (!schedules || schedules.length == 0) {
+                        schedule.update(req.body)
+                            .then(result => {
+                                if (result)
+                                    return res.status(200).send({ msg: 'Consulta alterada com sucesso' })
+                            })
+                            .catch(error => {
+                                return res.status(500).send({ msg: error })
+                            });
+                    }
+
+                    schedules.forEach(x => {
+                        if (x.dataValues.timeTable == req.body.timeTable)
+                            invalidTime = true;
+                    })
+
+                    if (invalidTime)
+                        return res.status(400).send({ msg: 'Nesse horário já existe uma consulta' });
+
+                    else if (!invalidTime) {
+                        Scheduling.update(req.body)
+                        .then(result => {
+                            if (result)
+                                return res.status(200).send({ msg: 'Consulta alterada com sucesso' });
+                        })
+                        .catch(error => {
+                            return res.status(500).send({ msg: error })
+                        });
+                    }
+                });
+            }
+
+            else {
+                schedule.update(req.body)
+                    .then(result => {
+                        if (result)
+                            return res.status(200).send({ msg: 'Consulta alterada com sucesso' })
+                    })
+                    .catch(error => {
+                        return res.status(500).send({ msg: error })
+                    });      
             }
         })
         .catch(error => res.status(500).send({ msg: error }));
