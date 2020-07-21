59884bf09c03b0f01f05779b8a84bf6de198c650
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-07-21 08:17:52 -0300

    fix the connection with mssql

--- src/config/database.js
+++ src/config/database.js
@@ -2,12 +2,14 @@ module.exports = {
     dialect: 'mssql',
     dialectOptions: {
         options: {
+            trustServerCertificate: true,
             useUTC: false,
             dateFirst: 1,
             encrypt: true   //use this for Azure database encryption
-        }
+        },
     },
-    host: '.,6455',
+    host: 'localhost',
+    port: 6455,
     username: 'sa',
     password: 'Senha_010203',
     database: 'DCApplication',
