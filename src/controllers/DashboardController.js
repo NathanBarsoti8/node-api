2f2e17a839ddc0cbf9f243d6263a6e591c52bd48
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-09-01 13:59:31 -0300

    method to send whatsapp message

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -1,7 +1,9 @@
 const Customer = require('../models/Customer');
 const Scheduling = require('../models/Scheduling');
 const ServiceType = require('../models/ServiceType');
+const Phone = require('../models/Phone');
 const Sequelize = require('sequelize');
+const sequelize = require('../config/sequelize');
 const { Op } = require('sequelize');
 const moment = require('moment'); 
 const literal = Sequelize.literal;
@@ -36,7 +38,7 @@ class DashboardController {
             attributes: ['date', 'timeTable'],
             include: [{
                 model: Customer,
-                attributes: ['name'],
+                attributes: ['name', 'id'],
                 required: true
             }, {
                 model: ServiceType,
@@ -61,6 +63,27 @@ class DashboardController {
         .catch(error => {
             return res.status(500).send({ msg: error })
         });
+    }
+
+    async sendWppMessage(req, res) {
+        let phone = await Phone.findOne({
+            attributes: ['id', 'ddd', 'phoneNumber'],
+            where: {
+                customerId: req.body.Customer.id,
+                typeId: 1
+            }
+        });
+
+        const baseUrl = "https://web.whatsapp.com/send?phone=";
+        const contact = `55${phone.ddd}${phone.phoneNumber}`;
+        const lineBreak = "%0A";
+        const asterisk = "%2A";
+        const tagText = "&text="
+        const text = `Olá, ${req.body.Customer.name}! Tudo bem? ${lineBreak} Você tem uma consulta às ${asterisk}${req.body.timeTable}${asterisk} no dia ${asterisk}${req.query.date}${asterisk} ${lineBreak} Podemos confirmar?`;
+
+        const link = `${baseUrl}${contact}${tagText}${text}`
+
+        return res.status(200).send({ wppLink: link });
     }
 
 }
