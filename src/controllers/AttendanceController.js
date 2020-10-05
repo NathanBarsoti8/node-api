a7fcf9159597eb4ad3e7c71ae5e1b41a5530aa98
Nathan Barsoti <nathanbarsoti@hotmail.com>
2020-10-05 06:57:30 -0300

    change update method

--- src/controllers/AttendanceController.js
+++ src/controllers/AttendanceController.js
@@ -10,7 +10,7 @@ class AttendanceController {
             attributes: ['id', 'name'],
             where: {
                 name: {
-                    [Op.like]: `%${req.body.name}%`
+                    [Op.like]: `${req.body.name}`
                 }
             }
         })
@@ -31,20 +31,42 @@ class AttendanceController {
         .catch(error => res.status(500).send({ msg: error }));
     }
 
-    update(req, res) {
-        ServiceType.findByPk(req.params.id)
-            .then(serviceType => {
-                if (serviceType) {
-                    serviceType.update(req.body)
+    async update(req, res) {
+        if (!req.params.id || req.params.id == null) 
+            return res.status(400).send({ msg: 'Não foi possível editar o tipo de serviço' });
+
+        await ServiceType.findAll({
+            attributes: ['id', 'name'],
+            where: {
+                name: {
+                    [Op.like]: `${req.body.name}`
+                }
+            }
+        })
+        .then(types => {
+            if (types.length > 0)
+                return res.status(400).send({ msg: 'Já existe um tipo de serviço com esse nome' });
+            else {
+                ServiceType.findOne({
+                    attributes: ['id', 'name'],
+                    where: {
+                        id: req.params.id
+                    }
+                })
+                .then(serviceType => {
+                    if (serviceType) {
+                        sequelize.query(` UPDATE [ServiceType] SET [name] = '${req.body.name}' WHERE [id] = ${req.params.id} `)
                         .then(update => {
                             if (update)
                                 return res.status(200).send({ msg: 'Tipo de serviço atualizado com sucesso'})
                             else
                                 return res.status(400).send({ msg: 'Ocorreu um erro ao editar tipo de serviço'});
                         })
-                }
-            })
-            .catch(error => res.status(500).send({ msg: error }));
+                    }
+                })
+                .catch(error => res.status(500).send({ msg: error }));
+            }
+        })
     }
 
     async delete(req, res) {
